0a72d9f1fb780406e64b35474a10b4856e01d438
Gabarros <gabarros17@gmail.com>
2020-02-04 18:46:13 -0300

    Add Main Component and styled

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-function main(){
-  return <Title> Main </Title>
+import { Container, Form, SubmitButton } from './styles';
+
+export default function main(){
+  return (
+    <Container>
+      <h1>
+      <FaGithubAlt />
+      Repositórios
+      </h1>
+      <Form onSubmit={() => {}}>
+        <input
+        type="text"
+        placeholder="Adicionar repositório"
+        />
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  )
 }
 
-export default main;
+
