4912e2d4265cd4c03a9a1b927bb5f5c486d77279
Gabarros <gabarros17@gmail.com>
2020-02-04 19:33:40 -0300

    create add new repository function

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,27 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import api from '../../services/api';
+
+
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import { Container, Form, SubmitButton } from './styles';
 
-export default function main(){
-  return (
-    <Container>
-      <h1>
-      <FaGithubAlt />
-      Repositórios
-      </h1>
-      <Form onSubmit={() => {}}>
-        <input
-        type="text"
-        placeholder="Adicionar repositório"
-        />
-        <SubmitButton>
-          <FaPlus color="#FFF" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  )
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: null,
+    error: null
+  }
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+    const { newRepo, repositories } = this.state;
+
+
+    const response = await api.get(`/repos/${newRepo}`).catch(err => {
+      this.setState({ error: true });
+      return;
+    });
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false
+    });
+
+
+
+  }
+
+  render() {
+    const { newRepo, loading, error } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+                <FaPlus color="#FFF" size={14} />
+              )}
+
+          </SubmitButton>
+          {error ? <p>Erro no nome do repositório!</p> : null}
+        </Form>
+      </Container>
+    )
+  }
+
 }
 
 
